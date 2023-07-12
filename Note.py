5b7ccd4dacb691fe0c83b8cda5bdb8f855d74f36
Nack <138799612+thinnaphatnack@users.noreply.github.com>
2023-07-13 05:30:58 +0700

    Update Note.py

--- Note.py
+++ Note.py
@@ -1,23 +1,41 @@
 องค์ประกอบพื้นฐานในภาษาเชิงวัตถุ
    1. ต้นแบบของวัตถุ(Class) & วัตถุ (Object)
 Object คือสิ่งที่ถูกสร้างขึ้นมาจาก Class ประกอบด้วย 2 คุณสมบัติคือ
-    ●คุณสมบัติ/คุณลักษณะ    
-        Attribute *คำนาม
-self.name = name
-    ●พฤติกรรม    
-        Behavior & Method (Function) *คำกริยา
-def getName(self):
+    ●คุณสมบัติ/คุณลักษณะ Attribute *คำนาม
+Ex. self.name = name
+    ●พฤติกรรม Behavior & Method (Function) *คำกริยา
+Ex. def getName(self):
 
 คุณสมบัติของการเขียนโปรแกรมเชิงวัตถุ
     1.การห่อหุ้ม   ( Encapsulation )
   Access Modifier ระดับในการเข้าถึง Class , Attribute , Method
   Public             : ทุก Class สามารถเข้าถึงได้
-  Protected (_) : Class ของตัวเองและ Class ลูกที่สืบทอดคุณสมบัติเท่านั้นที่จะเข้าถึงได้
-  Private ( __ )  : จะมีเฉพาะ Class ตัวเองเท่านั้นที่เข้าถึงได้
+  Protected (_)      : Class ของตัวเองและ Class ลูกที่สืบทอดคุณสมบัติเท่านั้นที่จะเข้าถึงได้
+  Private ( __ )     : จะมีเฉพาะ Class ตัวเองเท่านั้นที่เข้าถึงได้
   
-  Setter ,  Getter Method
+Setter ,  Getter (Method)
+   Setter การกำหนดค่าให้กับ Objet
+Ex. def SetName(self,newname):
+      self.__name = newname
+   Getter การดึงค่าจาก Object
+Ex. def getName(self):
+      return self.__name
 
-    2.การสืบทอด ( Inheritance )
+Class Variable & Instance Variable
+- Class Variable    คือ ตัวแปลที่ทำงานภายใน Class ส่วนอื่นสามารถเข้าถึงข้อมูลนี้ได้เลย ( Static attrbiute ) โดยไม่จำเป็นต้องสร้าง Object ขึ้นมา *เข้าถึงได้เฉพาะ Public และ Protected
+- Instance Variable คือ ตัวแปลที่อยู่ภายใน Object สามารถเข้าถึงข้อมูลส่วนนี้โดยการต้องสร้าง Object ขึ้นมา
+Ex.Class Employee :
+   # Class variable
+   _minSalary = 1000
+   _maxSalary = 5000
+      def __init__(self,name,salary,department):
+            # Instance Variable
+            self.__name = name
+            self.__salary = salary
+            self.__department = department
+
+    2.การสืบทอดคุณสมบัติ ( Inheritance )
+การสืบทอดคือ การสร้างสิ่งใหม่ขึ้นด้วยการสืบทอดหรือรับเอา(inherit)คุณสมบัติบางอย่างมาจากสิ่งเดิมที่มีอยู่แล้วโดยการสร้างเพิ่มเติมจากสิ่งที่มีอยู่แล้วได้เลย
     3.การพ้องรูป  ( Polymorphism )
 
 
